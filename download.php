e3e1d266f7473e44dee7654b142697135503ddbe
alcanceioficial-crypto <alcancei.oficial@gmail.com>
2025-11-28 14:58:36 -0300

    Update download.php

--- download.php
+++ download.php
@@ -1,15 +1,8 @@
 <?php
-$paymentId = $_GET["id"] ?? null;
 
-if(!$paymentId) exit("ID inválido");
+$file = "api/downloads/ebook.pdf";
 
-$pagamentos = file("pagamentos.txt", FILE_IGNORE_NEW_LINES);
-
-if(in_array($paymentId, $pagamentos)) {
-    header("Content-Type: application/pdf");
-    readfile("ebooks/exemplo.pdf");
-    exit;
-} else {
-    echo "Pagamento ainda não confirmado.";
-}
-?>
+header("Content-Type: application/pdf");
+header("Content-Disposition: attachment; filename=ebook.pdf");
+readfile($file);
+exit;
