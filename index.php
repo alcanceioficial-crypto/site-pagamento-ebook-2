8b5b6516cde2e867c6e492c942c95aa2e27a67c5
alcanceioficial-crypto <alcancei.oficial@gmail.com>
2025-11-28 11:24:38 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Ebook - Compra via PIX</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Ebook Exclusivo - R$ 2,00</h1>
+<p>Faça a compra via PIX e receba o download automaticamente.</p>
+
+<a href="checkout.php" class="btn">Comprar por R$ 2,00</a>
+
+</body>
+</html>
